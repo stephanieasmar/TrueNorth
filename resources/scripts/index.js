5c3549231315abc01b06a934943cbd404473d42c
Stephanie Asmar <Steph@StephansmarsMBP.attlocal.net>
2019-05-21 12:15:11 -0400

    navbar is fully responsive and collapse works

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -5,3 +5,13 @@ $('.menu-toggle').click(function() {
   
   })
   
+
+var top1 = $('#image').offset().top;
+var top2 = $('#text').offset().top;
+
+$(document).scroll(function() {
+  var scrollPos = $(document).scrollTop();
+  if (scrollPos >= top1 && scrollPos < top2) {
+    $('#nav-change').css('background-color', '##054347');
+  }
+});
